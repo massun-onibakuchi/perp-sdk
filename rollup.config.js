05609d67f86ec320c410a36f8a25e9e73d8be3c6
massun-onibakuchi <etherdao0x00@gmail.com>
2022-02-06 15:29:57 +0900

    chore: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
+
+const external = [
+    ...Object.keys(pkg.dependencies),
+    // ...Object.keys(pkg.peerDependencies),
+];
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                name: 'balancer-js',
+                file: pkg.browser,
+                format: 'umd',
+                sourcemap: true,
+                globals: {
+                    'graphql-request': 'graphqlRequest',
+                    'tiny-invariant': 'invariant',
+                    "graphql": 'graphql',
+                    "ethers": 'ethers',
+                },
+            },
+            { file: pkg.main, format: 'cjs', sourcemap: true },
+            { file: pkg.module, format: 'es', sourcemap: true },
+        ],
+        plugins: [nodeResolve(), json(), commonjs(), typescript()],
+        external,
+    },
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts(), typescript()],
+    },
+];
