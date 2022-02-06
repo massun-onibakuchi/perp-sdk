83895126eaeb4c07baf27cb2366dc23129f37d22
massun-onibakuchi <etherdao0x00@gmail.com>
2022-02-06 16:50:21 +0900

    chore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,11 @@
 import * as dotenv from 'dotenv'
 import { HardhatUserConfig } from 'hardhat/config'
-import '@typechain/hardhat'
 
 dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: '0.7.6',
   networks: {},
-  typechain: {
-    outDir: 'src/abi/types',
-  },
 }
 
 export default config
